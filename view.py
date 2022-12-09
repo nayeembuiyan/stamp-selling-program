f3fb88b2a22ee01677badcd83b774390cb850746
Nayeem Bhuiyan <86225519+nayeembuiyan@users.noreply.github.com>
2022-12-09 20:10:39 +1100

    Delete view.py

--- view.py
+++
@@ -1,8 +0,0 @@
-def view(order):
-    count = 1
-    subtotal = 0
-    for j in order:
-        print(f"{count:{2}}.  {j:{30}}  {order[j][0]:{10}}")  # Printing item ID and item cost
-        subtotal += order[j][0]
-        count += 1
-    print("\nTotal Cost: ", subtotal)
